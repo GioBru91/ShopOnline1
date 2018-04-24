505aae97f43647374900ba4db0b32a358b2e1d0f
GioBru91 <giovannibrugaletta20@gmail.com>
2018-04-24 12:23:29 +0200

    utilities

---
+++ ShopOnline1/src/it/accenture/utilities/CostantiConnessione.java
@@ -0,0 +1,9 @@
+package it.accenture.utilities;
+
+public class CostantiConnessione {
+	
+	public static final String DB_USERNAME = "shoponline";
+	public static final String DB_PASSWORD = "shoponline";
+	public static final String DB_DRIVER = "oracle.jdbc.driver.OracleDriver";
+	public static final String DB_URL = "jdbc:oracle:thin:@localhost:1521:orcl";
+}
