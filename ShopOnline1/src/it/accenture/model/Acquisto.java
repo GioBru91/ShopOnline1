0ac395b1843a6ebd693441f4fab303c337d2ea04
GioBru91 <giovannibrugaletta20@gmail.com>
2018-04-24 14:30:07 +0200

    asd

---
+++ ShopOnline1/src/it/accenture/model/Acquisto.java
@@ -0,0 +1,15 @@
+package it.accenture.model;
+
+import java.util.Date;
+
+public class Acquisto {
+
+	private int idAcquisto;
+	private Spedizione tipoSpedizione;
+	private Date dataInizio;
+	private Date dataFine;
+	private double prezzoDiSpedizione;
+	private int quantitaAcquistata;
+	
+	
+}
---
+++ ShopOnline1/src/it/accenture/model/Categoria.java
@@ -0,0 +1,12 @@
+package it.accenture.model;
+
+public enum Categoria {
+
+	ABBIGLIAMENTO_UOMO,
+	ABBIGLIAMENTO_DONNA,
+	ABBIGLIAMENTO_BAMBINO,
+	CASA,
+	ELETTRONICA,
+	LIBRI;
+	
+}
