10ca1219d935d8acebc877e0f26455d3942beca1
GioBru91 <giovannibrugaletta20@gmail.com>
2018-04-26 11:37:37 +0200

    gestioneform

---
+++ ShopOnline1/WebContent/js/gestioneForm.js
@@ -0,0 +1,64 @@
+function validazioneForm() {
+	var nome=  document.getElementById('nome').value;
+	var cognome=  document.getElementById('cognome').value;
+	var username=  document.getElementById('username').value;
+	var password=  document.getElementById('password').value;
+	var controllo= true;
+	
+	if (nome==""){
+		document.getElementById('nome').placeholder='Campo Obbligatorio';
+		document.getElementById('nome').classList.add('alert-danger');
+		controllo=false;
+	}	
+	
+	if (cognome==""){
+		document.getElementById('cognome').placeholder='Campo Obbligatorio';
+		document.getElementById('cognome').classList.add('alert-danger');
+		controllo=false;
+	}
+	if (username==""){
+		document.getElementById('username').placeholder='Campo Obbligatorio';
+		document.getElementById('username').classList.add('alert-danger');
+		controllo=false;
+	}
+	if (password==""){
+		document.getElementById('password').placeholder='Campo Obbligatorio';
+		document.getElementById('password').classList.add('alert-danger');
+		controllo=false;
+	}
+	if(controllo==false){
+		document.getElementById('alert').style.display='block';
+	}
+
+	return controllo;
+}
+
+function validazioneFormLogin() {
+	var username=  document.getElementById('username').value;
+	var password=  document.getElementById('password').value;
+	var controllo= true;
+	
+	if (username=="" & password ==""){
+		document.getElementById('username').placeholder='Campo Obbligatorio';
+		document.getElementById('username').classList.add('alert-danger');
+		document.getElementById('password').placeholder='Campo Obbligatorio';
+		document.getElementById('password').classList.add('alert-danger');
+		controllo=false;
+	}else if(username=="" & password !=""){
+		document.getElementById('username').placeholder='Campo Obbligatorio';
+		document.getElementById('username').classList.add('alert-danger');
+		document.getElementById('password').classList.remove('alert-danger');
+		document.getElementById('password').classList.add('alert-success');
+		controllo=false;
+	}
+	else if (username !="" & password==""){
+		document.getElementById('username').classList.remove('alert-danger');
+		document.getElementById('username').classList.add('alert-success');
+		document.getElementById('password').placeholder='Campo Obbligatorio';
+		document.getElementById('password').classList.add('alert-danger');
+		controllo=false;
+	
+	}
+		return controllo;
+}
+
