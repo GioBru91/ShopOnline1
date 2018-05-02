917acc7981f99b12e686ce99ee147b271fafce49
GioBru91 <giovannibrugaletta20@gmail.com>
2018-05-02 14:46:29 +0200

    aaa

--- ShopOnline1/src/it/accenture/controller/Carrello.java
+++ ShopOnline1/src/it/accenture/controller/Carrello.java
@@ -1,16 +1,28 @@
 package it.accenture.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import it.accenture.model.Prodotto;
+import it.accenture.model.Utente;
 
 public class Carrello extends HttpServlet{
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		Utente utenteLoggato = (Utente) req.getAttribute("utenteLoggato");
+		
+		int idProdotto = Integer.parseInt(req.getParameter("idProdotto"));
+		
+		List<Prodotto> listaCarrello = (List<Prodotto>) session.getAttribute("listaCarrello");
+		
 		
 		
 	}
